f5a30af791fba1df5081b87043d76ae5424c11ef
woekwoek-IBMO <ibrahim.mohammed@laetottenham.ac.uk>
2024-10-17 10:56:06 +0000

    Pending changes exported from your codespace

--- currency_exchange_tool.py
+++ currency_exchange_tool.py
@@ -1,13 +1,35 @@
 import math # you'll probably need this
+import requests
 
-exchange_rates = {
-    'USD': 1.13, #I.E. 1 Pound is 1.13 Dollars
-    'EUR': 1.15,
-}
+url = "https://v6.exchangerate-api.com/v6/581c3caead82c23e72b538e3/latest/GBP"
+
+response = requests.get(url)
+data=response.json()
+data = data["conversion_rates"]
+
+
+#exchange_rates = {
+    #'GBP' : 1,
+    #'USD': 1.31,
+    #'EUR': 1.19,
+    #'CAD' : 1.8,
+    #'AUD' : 1.94,
+    #'JPY' : 195.21
+#}
 
 def check_currency_exists(currency):
-    return
+    return currency in data.keys()
 
 def currency_convert(original_c, new_c, amount):
-    # your code here
-    return
+    newAmount = amount
+    if check_currency_exists(original_c):
+        newAmount /= data[original_c]
+    if check_currency_exists(new_c):
+        newAmount *= data[new_c]
+    return newAmount
+
+old_currency="GBP"
+new_currency=input("What currency would you like to convert to?").upper()
+amount=int(input("How much?"))
+new= currency_convert(old_currency,new_currency,amount)
+print (round(new,2))
